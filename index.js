d1ae5993a6f73d991ae2e8f83bf1323e2fb8c894
Zeyrox <154805566+ZeyroxKabir@users.noreply.github.com>
2024-08-26 02:44:04 -0700

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Watching Bandidos","Playing Vmp"];
 
 
 let currentIndex = 0;
